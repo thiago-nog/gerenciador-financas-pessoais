c98a0250b9f40fea4a31a2886c9d199b3b4831d1
miryarioss <129385923+miryarioss@users.noreply.github.com>
2023-04-24 20:19:36 -0300

    Update despesas.sql

--- bd/despesas.sql
+++ bd/despesas.sql
@@ -7,3 +7,15 @@ CREATE TABLE despesas (
     usuario_id INTEGER NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
+
+CREATE TABLE despesas (
+  id INTEGER NOT NULL UNIQUE,
+  valor MONEY NOT NULL,
+  data_vencimento DATE NOT NULL,
+  tipo_despesa_id INTEGER NOT NULL,
+  descricao VARCHAR(100) NOT NULL,
+  usuario_id INTEGER NOT NULL,
+  
+  PRIMARY KEY (id),
+  FOREIGN KEY (tipo_despesa_id, usuario_id)
+);
