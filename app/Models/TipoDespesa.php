a3e70e03ca0ff8b8f1f165c67b9460a1ddd2462a
Fernando Mattioli <mattioli.fernando@gmail.com>
2023-05-05 20:06:27 -0300

    Fix #35: acrescenta o modelo TipoDespesa

---
+++ app/Models/TipoDespesa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoDespesa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tipos_despesa';
+}
