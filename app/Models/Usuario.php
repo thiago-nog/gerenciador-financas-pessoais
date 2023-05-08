12285ab95783f5449236bab6dc0665c65a88a71e
Pierre Mendes Salatiel <pierretielmendes@gmail.com>
2023-05-08 12:37:35 -0300

    feat: adiciona implementação model, seeder, factory de Usuari.

---
+++ app/Models/Usuario.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [ 'id', 'login', 'password' ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
---
+++ database/factories/UsuarioFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Usuario>
+ */
+class Usuario extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'login' => fake()->unique()->safeEmail(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ];
+    }
+}
