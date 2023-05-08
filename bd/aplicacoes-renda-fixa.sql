3708797db46575de71bd18bfa5e76f9c35d83aad
Fernando Mattioli <mattioli.fernando@gmail.com>
2023-05-08 19:17:18 -0300

    Fix #1: cria aplicacoes_renda_fixa

--- bd/aplicacoes-renda-fixa.sql
+++ bd/aplicacoes-renda-fixa.sql
@@ -4,5 +4,5 @@ CREATE TABLE aplicacoes_renda_fixa (
     data_aplicacao DATE NOT NULL,
     valor_atual MONEY NOT NULL,
     usuario_id INTEGER NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) 
 );
