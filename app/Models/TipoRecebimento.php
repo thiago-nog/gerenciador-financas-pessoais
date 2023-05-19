63004021dccebcc125c8bfa7647b0dddc92c4156
DSilvaNunes <doug.silvin@gmail.com>
2023-05-19 20:30:02 -0300

    fix issue #47 and 48

--- app/Models/TipoRecebimento.php
+++ app/Models/TipoRecebimento.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class TipoRecebimento extends Model
 {
     use HasFactory;
+    protected $table = 'tipos_recebimento';
+
 }
--- database/migrations/2023_05_12_230500_create_tipos_recebimento.php
+++ database/migrations/2023_05_12_230500_create_tipos_recebimento.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tipos_recebimentos', function (Blueprint $table) {
+        Schema::create('tipos_recebimento', function (Blueprint $table) {
             $table->id();
             $table->string('descricao')->unique();
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tipos_recebimentos');
+        Schema::dropIfExists('tipos_recebimento');
     }
 };
